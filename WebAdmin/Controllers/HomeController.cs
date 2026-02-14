3b446d70c0f156774164b8532b54e581594c054a
ivanIRP <ivatar1066@gmail.com>
2026-02-13 19:22:59 -0600

    correccion en la paguina

--- WebAdmin/Controllers/HomeController.cs
+++ WebAdmin/Controllers/HomeController.cs
@@ -1,1 +1,27 @@
-zz
+using Microsoft.AspNetCore.Mvc;
+using ElectronicsStoreWeb.Services;
+
+namespace ElectronicsStoreWeb.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ApiService _apiService;
+
+        public HomeController(ApiService apiService)
+        {
+            _apiService = apiService;
+        }
+
+        public async Task<IActionResult> Index()
+        {
+            var reporte = await _apiService.GetReporteVentasAsync();
+            var productos = await _apiService.GetProductosAsync();
+            
+            ViewBag.Reporte = reporte;
+            ViewBag.TotalProductos = productos.Count;
+            ViewBag.ProductosBajoStock = productos.Count(p => p.Cantidad < 5);
+            
+            return View();
+        }
+    }
+}
